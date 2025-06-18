4660aede45516aaef433e103d715edf1d5b3de60
Nguyen Van Duc <duc.nguyenvan@vietis.com.vn>
2025-06-18 09:27:20 +0700

    add app 1

---
+++ app1/Unit1.pas
@@ -0,0 +1,25 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TForm1 = class(TForm)
+  private
+    { Private éŒ¾ }
+  public
+    { Public éŒ¾ }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
+ 
